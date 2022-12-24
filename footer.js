57439e61da321710aebe7334e8093a18adbb4b05
KotoriAozora <kotorin.nozomin.hononon.0912@gmail.com>
2022-12-25 08:22:41 +0900

    パスの名前の変更

--- footer.js
+++ footer.js
@@ -1,11 +1,11 @@
 document.write(
 	'<footer>' +
-	'<img id="amazon" src="./img/Amazon-logo-RGB.png" alt="Amazon brand logo">' +
+	'<img id="amazon" src="https://aozorakotori.github.io/kotori-note/images/Amazon-logo-RGB.png" alt="Amazon brand logo">' +
 	'<span style="display: inline">' +
-	'Amazonウィッシュリスト（ほしい物リスト）を公開しています。よろしければご支援ください。' +
-	'ご協力ありがとうございます。' +
+	'Amazonウィッシュリスト（ほしい物リスト）を公開しています。<br>' +
+	'よろしければご支援ください。ご協力ありがとうございます。' +
 	'</span>' +
-	'<div class="center">' +
+	'<div class="center" style="padding:1em 0 1em 0">' +
 	'© 2020-2022 蒼空ことり All rights reserved.' +
 	'</div>' +
 	'</footer>'
