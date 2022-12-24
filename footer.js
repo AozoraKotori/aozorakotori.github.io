47c6295ea321b642983e557847dc88831f0fd98e
KotoriAozora <kotorin.nozomin.hononon.0912@gmail.com>
2022-12-24 22:35:23 +0900

    footer に Amazon 関連を追加中

--- footer.js
+++ footer.js
@@ -1,1 +1,12 @@
-document.write('<footer><div class="center">© 2020-2022 蒼空ことり All rights reserved.</div></footer>')
+document.write(
+	'<footer>' +
+	'<img id="amazon" src="./img/Amazon-logo-RGB.png" alt="Amazon brand logo">' +
+	'<span style="display: inline">' +
+	'Amazonウィッシュリスト（ほしい物リスト）を公開しています。よろしければご支援ください。' +
+	'ご協力ありがとうございます。' +
+	'</span>' +
+	'<div class="center">' +
+	'© 2020-2022 蒼空ことり All rights reserved.' +
+	'</div>' +
+	'</footer>'
+)
