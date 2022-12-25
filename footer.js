72447be833514bc6b4b61ea8411f5b75722a8128
KotoriAozora <kotorin.nozomin.hononon.0912@gmail.com>
2022-12-25 21:21:39 +0900

    AmazonのロゴをSVGにした

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 document.write(
 	'<footer>' +
 	'<div class="container">' +
-	'<img id="amazon" src="https://aozorakotori.github.io/kotori-note/images/Amazon-logo.png" alt="Amazon logo">' +
+	'<img id="amazon" src="https://aozorakotori.github.io/kotori-note/images/Amazon-logo.svg" alt="Amazon logo">' +
 	'Amazonウィッシュリスト（ほしい物リスト）を公開しています。<br>' +
 	'よろしければご支援ください。ご協力ありがとうございます。' +
 	'<div class="center" style="padding:1em 0 0 0">' +
