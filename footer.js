dae4c669c26d7ed341a558a45f6d903521290f01
KotoriAozora <kotorin.nozomin.hononon.0912@gmail.com>
2022-12-26 00:05:46 +0900

    ボタンが，できたぞ……！

--- footer.js
+++ footer.js
@@ -1,10 +1,10 @@
 document.write(
 	'<footer>' +
-	'<div class="container">' +
+	'<div class="container" style="font-size:16px">' +
 	'<img id="amazon" src="https://aozorakotori.github.io/kotori-note/images/Amazon-logo.svg" alt="Amazon logo">' +
 	'Amazonウィッシュリスト（ほしい物リスト）を公開しています。<br>' +
 	'よろしければご支援ください。ご協力ありがとうございます。' +
-	'<div class="center" style="padding:1em 0 0 0">' +
+	'<div class="center" style="padding-top:1em">' +
 	'© 2020-2022 蒼空ことり All rights reserved.' +
 	'</div>' +
 	'</div>' +
