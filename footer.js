8c5d30c278c4e21fef97141223b3aa47d1df012a
KotoriAozora <kotorin.nozomin.hononon.0912@gmail.com>
2022-12-26 02:55:29 +0900

    軽微な変更

--- footer.js
+++ footer.js
@@ -3,7 +3,7 @@ document.write(
 	'<div class="container" style="font-size:16px">' +
 	'<img id="amazon" src="https://aozorakotori.github.io/kotori-note/images/Amazon-logo.svg" alt="Amazon logo">' +
 	'<span class="center buttonA buttonAb" style="float:right">' +
-	'<a href="https://www.amazon.co.jp/hz/wishlist/ls/2NFPC04NO7T7K?ref_=wl_share" style="width:300px" target="_blank" rel="noopener noreferrer"><span>ほしい物リスト</span></a>' +
+	'<a href="https://www.amazon.co.jp/hz/wishlist/ls/2NFPC04NO7T7K?ref_=wl_share" style="width:300px" target="_blank" rel="noopener noreferrer"><span>ウィッシュリスト</span></a>' +
 	'</span>' +
 	'Amazonウィッシュリスト（ほしい物リスト）を公開しています。<br>' +
 	'よろしければご支援ください。ご協力ありがとうございます。' +
