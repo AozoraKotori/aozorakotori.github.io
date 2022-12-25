aa2fc721263fbf20c9a88619bc6a7fe214df7161
KotoriAozora <kotorin.nozomin.hononon.0912@gmail.com>
2022-12-25 15:19:46 +0900

    logo の拡充 footer の幅の調整

--- footer.js
+++ footer.js
@@ -1,10 +1,10 @@
 document.write(
 	'<footer>' +
+	'<div>' +
 	'<img id="amazon" src="https://aozorakotori.github.io/kotori-note/images/Amazon-logo-RGB.png" alt="Amazon brand logo">' +
-	'<span style="display: inline">' +
 	'Amazonウィッシュリスト（ほしい物リスト）を公開しています。<br>' +
 	'よろしければご支援ください。ご協力ありがとうございます。' +
-	'</span>' +
+	'</div>' +
 	'<div class="center" style="padding:1em 0 1em 0">' +
 	'© 2020-2022 蒼空ことり All rights reserved.' +
 	'</div>' +
