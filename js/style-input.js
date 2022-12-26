32a4dbc00bb833be038a710bbfe29ddd5dcc0b46
KotoriAozora <kotorin.nozomin.hononon.0912@gmail.com>
2022-12-26 10:51:10 +0900

    フォルダ階層を整理

---
+++ js/style-input.js
@@ -0,0 +1,11 @@
+document.write(
+	'<meta charset="UTF-8">' +
+	'<meta name="author" content="蒼空ことり(@Kotori_Aozora_M)">' +
+	'<link rel="stylesheet" href="https://aozorakotori.github.io/kotori-note/css/style.css">' +
+	'<link rel="stylesheet" href="https://aozorakotori.github.io/kotori-note/css/button.css">' +
+	'<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>' +
+	'<link rel="preconnect" href="https://fonts.googleapis.com">' +
+	'<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' +
+	'<link href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic:wght@400;700&display=swap" rel="stylesheet">' +
+	'<style>@import url("https://fonts.googleapis.com/css2?family=BIZ+UDPGothic:wght@400;700&display=swap");</style>'
+)
