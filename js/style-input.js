db0410355edb3e4f2b6ae9a003636607d574324d
KotoriAozora <kotorin.nozomin.hononon.0912@gmail.com>
2022-12-28 20:33:10 +0900

    google analyticsの実装

--- js/style-input.js
+++ js/style-input.js
@@ -5,6 +5,14 @@ document.write(
 	'<link rel="stylesheet" href="https://aozorakotori.github.io/kotori-note/css/style.css">' +
 	'<link rel="stylesheet" href="https://aozorakotori.github.io/kotori-note/css/button.css">' +
 	'<script src="https://code.jquery.com/jquery-3.6.3.min.js"></script>' +
+	'< !--Google tag(gtag.js)-- >' +
+	'<script async src="https://www.googletagmanager.com/gtag/js?id=G-18CXH1ZBLW"></script>' +
+	'<script>' +
+	'	window.dataLayer = window.dataLayer || [];' +
+	'function gtag() { dataLayer.push(arguments); }' +
+	'gtag("js", new Date());' +
+	'gtag("config", "G-18CXH1ZBLW");' +
+	'</script>' +
 	'<link rel="preconnect" href="https://fonts.googleapis.com">' +
 	'<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' +
 	'<link href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic:wght@400;700&display=swap" rel="stylesheet">' +
