739ff3fd92dc0d7926b12c2dac2077c3f6e87241
KotoriAozora <kotorin.nozomin.hononon.0912@gmail.com>
2022-12-28 21:56:15 +0900

    jQueryテストその３

--- js/header.js
+++
@@ -1,7 +0,0 @@
-document.write(
-	'<header>' +
-	'<div class="container">' +
-	'<a href="https://aozorakotori.github.io/kotori-note/" style="text-decoration:none; color:black">ことりノート</a>' +
-	'</div>' +
-	'</header>'
-);
